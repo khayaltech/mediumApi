0bf7e1c7e1b6ba8a23d801ae2654e56c888e574a
khayaltech <khayaltech@gmail.com>
2022-11-20 18:48:45 +0400

    add celery, redis and flower

--- mediumApi/settings/local.py
+++ mediumApi/settings/local.py
@@ -14,3 +14,16 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 # SECRET_KEY = 'django-insecure-uz(cebeajnd7jekvt(hh*s-xbs9=a0mds))r+0)5g@!4*7(7i@'
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER ='khayalfarajov@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Medium Api"
+DEFAULT_FROM_EMAIL = "khayalfarajov@gmail.com"
